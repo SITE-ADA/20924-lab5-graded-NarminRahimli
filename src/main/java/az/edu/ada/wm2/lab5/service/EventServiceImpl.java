f68f59d1b394dd65f95be557b97b679f38442193
NarminRahimli <nrahimli19583@ada.edu.az>
2026-02-26 15:47:33 +0400

    AddEventService:{getUpcomingEvents} implementation

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -97,6 +97,16 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+    public List<Event> getUpcomingEvents() {
+        LocalDateTime now = LocalDateTime.now();
+
+        return eventRepository.findAll().stream()
+                .filter(event -> event.getEventDateTime() != null &&
+                        event.getEventDateTime().isAfter(now))
+                .collect(Collectors.toList());
+    }
+
+    @Override
     public List<Event> getEventsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice) {
        return List.of();
     }
